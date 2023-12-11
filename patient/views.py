232a538ebec11a51ad22bad9f0c6525f452c9445
MdZahidulIslam-Bismi <mdzahidulislam0184@gmail.com>
2023-12-10 19:21:35 -0500

    Query Integrity

--- patient/views.py
+++ patient/views.py
@@ -1,24 +1,46 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .models import Patient
-from pprint import pprint
-
-# Create your views here.
+from .query import query_patients_by_weight_range
 
 
 def home(request):
-
     if request.user.is_authenticated:
         groups = [group.name for group in request.user.groups.all()]
-        print(groups)
-        if 'H' in groups:
-            patients = Patient.objects.all()
-            return render(request, 'index.html', {'patients': patients})
-        if 'R' in groups:
-            patients = Patient.objects.all().exclude(first_name=None, last_name=None)
-            return render(request, 'index.html', {'patients': patients})
+        patients_data = []
+
+        min_weight = request.GET.get('min_weight', None)
+        max_weight = request.GET.get('max_weight', None)
+
+        if min_weight and max_weight and ('H' in groups or 'R' in groups):
+            # Convert to integers if needed
+            min_weight = int(min_weight)
+            max_weight = int(max_weight)
+            patients = query_patients_by_weight_range(min_weight, max_weight)
+
+            if 'R' in groups:
+                patients = patients.exclude(first_name=None, last_name=None)
+            return render(request, 'index.html', {'patients': patients_data})
+
         return render(request, 'index.html', {'error': 'You are not authorized to view this page'})
     return redirect("login")
+
+
+def weight_range(request):
+    # Example range values, you can modify this to get the range from the request
+    min_weight = request.GET.get('min_weight', None)
+    max_weight = request.GET.get('max_weight', None)
+
+    if min_weight and max_weight:
+        # Convert to integers if needed
+        min_weight = int(min_weight)
+        max_weight = int(max_weight)
+        patients = query_patients_by_weight_range(min_weight, max_weight)
+    else:
+        # Return an empty queryset if no valid range is provided
+        patients = Patient.objects.none()
+
+    return render(request, 'index.html', {'patients': patients})
 
 
 def user_login(request):
